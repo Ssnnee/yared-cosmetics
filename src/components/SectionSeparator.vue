b70803c39cf411934cff1a066edb96fecb1c4066
Samuel Nandi <nandisamuelsne@gmail.com>
2025-03-07 02:23:31 +0100

    feat: add a component to facilitate section separation

---
+++ src/components/SectionSeparator.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+
+defineProps<{ sectionTitle: string }>()
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center">
+    <div class="border-b-1 border-foreground w-1/2"></div>
+    <h2 class="text-4xl my-10">{{ sectionTitle }}</h2>
+  </div>
+</template>
