360f92cb3b133755e3651f9a11a6d9acf03539a2
Samuel Nandi <nandisamuelsne@gmail.com>
2025-03-07 01:21:29 +0100

    feat: add the footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div className="flex items-center justify-between px-14">
+    <div class="flex items-center w-full space-x-4">
+      <div class="border-b-1 border-foreground w-1/2"></div>
+      <a href="" class="">
+        <img class="w-8" src="/logo.png" alt="" />
+      </a>
+      <div class="border-b border-foreground w-1/2"></div>
+      </hr>
+    </div>
+  </div>
+  <footer className="flex items-center justify-between px-14">
+    <p>© {{ new Date().getFullYear() }} Yared Cosmetecs</p>
+    <div class="flex items-center space-x-4">
+    </div>
+  </footer>
+</template>
+
