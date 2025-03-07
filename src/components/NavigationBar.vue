4fb6d789d61bd3905db98f5bc082ccfca1fd7c98
Samuel Nandi <nandisamuelsne@gmail.com>
2025-03-07 01:19:29 +0100

    feat: add navigation bar

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+ const navigation = ref([
+    { name: 'Home', href: '/home' },
+    { name: 'About', href: '/about' },
+    { name: 'Contact', href: '/contact' },
+  ])
+</script>
+
+<template>
+  <header class="py-4 border-b">
+    <nav class="flex container items-center">
+      <a href="" class="flex gap-1 items-center">
+        <img class="w-14" src="/logo.png" alt="" />
+        <span class="text-3xl font-bold">Nuxtcipes</span>
+      </a>
+      <ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
+        <li>
+          <a href="">Home</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+
