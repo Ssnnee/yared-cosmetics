708300c093394fee86f491363ba5998405e5bfa5
Samuel Nandi <nandisamuelsne@gmail.com>
2025-03-12 23:33:12 +0100

    feat: take care of responsive and add animation to the buttons
    This change make the menu to show off on small screen add the animation
    scale the button.

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,19 +1,42 @@
 <script setup lang="ts">
-  const navItems = ["Accueil","Produit", "A propos" , "Contact",];
+import { ref } from 'vue';
+
+const navItems = ["Accueil", "Produit", "A propos", "Contact"];
+const isMenuOpen = ref(false);
 </script>
 
 <template>
   <header class="py-4 px-14">
-    <nav class="flex flex-center justify-between ">
-      <a href="" class="">
-        <img class="w-14" src="/logo.png" alt="" />
+    <nav class="flex justify-between items-center">
+      <a href="">
+        <img class="w-20" src="/logo.png" alt="Logo" />
       </a>
-      <ul class="flex flex-center space-x-4 ">
+
+      <!-- Desktop Menu -->
+      <ul class="hidden md:flex space-x-6">
         <li v-for="item in navItems" :key="item">
-          <a :href="'#' + item">{{ item }}</a>
+          <a :href="'#' + item" class="hover:text-gray-600">{{ item }}</a>
         </li>
       </ul>
+
+      <button @click="isMenuOpen = !isMenuOpen" class="md:hidden cursor-pointer">
+        <img v-if="!isMenuOpen" class="w-8 flex dark:hidden" src="/icons/menu.svg" alt="Menu" />
+        <img v-if="!isMenuOpen" class="w-8 hidden dark:flex" src="/icons/menu-light.svg" alt="Menu" />
+        <img v-if="isMenuOpen" class="w-8 flex dark:hidden" src="/icons/close.svg" alt="Menu" />
+        <img v-if="isMenuOpen" class="w-8 hidden dark:flex" src="/icons/close-light.svg" alt="Menu" />
+      </button>
     </nav>
+
+    <!-- Mobile Menu -->
+    <div v-if="isMenuOpen" class="md:hidden mt-4 transition-all delay-1000 absolute">
+      <ul class="flex flex-col space-y-4 p-4 shadow-lg">
+        <li v-for="item in navItems" :key="item">
+          <button @click="isMenuOpen = !isMenuOpen" class="cursor-pointer">
+            <a :href="'#' + item" class="hover:text-gray-600">{{ item }}</a>
+          </button>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
