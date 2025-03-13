9433c9ed64ef26f37cfc2f27c815ad56425f576e
Samuel Nandi <nandisamuelsne@gmail.com>
2025-03-13 02:59:29 +0100

    feat: adjust border colors to be more elegant or different mode

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,11 +4,11 @@
 <template>
   <div className="flex items-center justify-between px-14">
     <div class="flex items-center w-full space-x-4">
-      <div class="border-b-1 border-foreground w-1/2"></div>
+      <div class="border-b dark:border-border-d w-1/2"></div>
       <a href="" class="">
         <img class="w-8" src="/logo.png" alt="" />
       </a>
-      <div class="border-b border-foreground w-1/2"></div>
+      <div class="border-b dark:border-border-d w-1/2"></div>
       </hr>
     </div>
   </div>
--- src/components/SectionSeparator.vue
+++ src/components/SectionSeparator.vue
@@ -6,7 +6,7 @@ defineProps<{ sectionTitle: string }>()
 
 <template>
   <div class="flex flex-col items-center justify-center">
-    <div class="border-b-1 border-foreground w-1/2"></div>
+    <div class="border-b dark:border-border-d w-1/2"></div>
     <h2 class="text-4xl my-10">{{ sectionTitle }}</h2>
   </div>
 </template>
